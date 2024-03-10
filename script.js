7c81ed64dc976fb3da36547d94ae5d761187532b
NicolasIppoliti <contacto.nicolasippoliti@gmail.com>
2024-03-10 17:19:57 -0300

    refactor JS functions

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function slider(anything) {
-    document.querySelector('.nespresso').src = anything;
+const circle = document.querySelector(".circle");
+const textColor = document.querySelector(".brand-name");
+const buttonColor = document.querySelector(".learn-more");
+const nespresso = document.querySelector(".nespresso");
+
+async function slider(anything) {
+  nespresso.src = anything;
 }
 
-function changeColor(color) {
-    const circle = document.querySelector('.circle');
-    const textColor = document.querySelector('.brand-name');
-    const buttonColor = document.querySelector('.learn-more');
-    circle.style.background = color;
-    textColor.style.color = color;
-    buttonColor.style.background = color;
+async function changeColor(color) {
+  circle.style.background = color;
+  textColor.style.color = color;
+  buttonColor.style.background = color;
 }
